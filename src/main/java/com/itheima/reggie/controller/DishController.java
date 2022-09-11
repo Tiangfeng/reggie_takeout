d154b25e7c09949109fd67b0fcad968f08cc460d
tianfengli <istianfengl@gmail.com>
2022-09-11 13:27:48 +0800

    整合SpringCache缓存， 为前端套餐数据的展示加入缓存。

--- src/main/java/com/itheima/reggie/controller/DishController.java
+++ src/main/java/com/itheima/reggie/controller/DishController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
